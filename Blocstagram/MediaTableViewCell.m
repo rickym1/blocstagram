899ede68e66fa6a59d3529294fceafda73b68f58
Rick Matta <rickmatta@gmail.com>
2015-07-08 16:13:28 -0400

    a fix?

--- Blocstagram/MediaTableViewCell.m
+++ Blocstagram/MediaTableViewCell.m
@@ -64,7 +64,7 @@ static NSParagraphStyle *paragraphStyle;
     if (self.mediaItem.image.size.width > 0 && CGRectGetWidth(self.contentView.bounds) > 0) {
         self.imageHeightConstraint.constant = self.mediaItem.image.size.height / self.mediaItem.image.size.width * CGRectGetWidth(self.contentView.bounds);
     } else {
-        self.imageHeightConstraint.constant = 0;
+        self.imageHeightConstraint.constant = self.mediaItem.image.size.height / self.mediaItem.image.size.width * 2;
     }
     
     // Hide the line between cells
