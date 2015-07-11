39cdfc35f72fcc634fa1ce54fc1a0b74e307d242
Rick Matta <rickmatta@gmail.com>
2015-07-11 17:54:08 -0400

    updated some

--- Blocstagram/ComposeCommentView.m
+++ Blocstagram/ComposeCommentView.m
@@ -121,7 +121,7 @@
 
 - (void) animateWithDuration {
     [UIView animateWithDuration:1 delay:0 usingSpringWithDamping:0.5 initialSpringVelocity:1 options:0 animations:^{
-        primaryConstrain.constant = 0;
+        primaryConstraint.constant = 0;
         [self.view button];
     }completion:nil];
 }
